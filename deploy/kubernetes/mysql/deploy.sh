bd60948c513ecd1a9fe27ed91d8afcab40df8ffe
jccastellanos-asd <jccastellanos@grupoasd.com>
2025-05-26 14:08:12 -0500

    Update pvc

--- deploy/kubernetes/mysql/deploy.sh
+++ deploy/kubernetes/mysql/deploy.sh
@@ -1,6 +1,7 @@
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-configmap.yaml
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-secret.yaml
-kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-pv.yaml
-kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-pvc.yaml
+# kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-pv.yaml
+# kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-pvc.yaml
+kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-pvc-dynamic.yaml
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-deployment.yaml
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python apply -f mysql-service.yaml
--- deploy/kubernetes/mysql/undeploy.sh
+++ deploy/kubernetes/mysql/undeploy.sh
@@ -1,10 +1,6 @@
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python delete service mysql-service
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python delete deployment mysql-app
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python delete pvc mysql-pvc
-kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python delete pv mysql-pv
+# kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python delete pv mysql-pv
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python delete secret mysql-secret
 kubectl --kubeconfig ../config.yaml --namespace demo-contry-api-python delete configmap mysql-config
-
-
-
-
